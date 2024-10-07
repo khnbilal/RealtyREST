8458e38bb2c2afc4f87197b033c7f3da8df462e3
Bilal Khan <khn.bilal17@gmail.com>
2024-10-07 14:32:04 +0530

    Create AddProperty.js

---
+++ Frontend/app/components/AddProperty.js
@@ -0,0 +1,12 @@
+import AddPropertyForm from '../components/AddPropertyForm';
+
+const AddProperty = () => {
+  return (
+    <div>
+      <h1>Add New Property</h1>
+      <AddPropertyForm />
+    </div>
+  );
+};
+
+export default AddProperty;

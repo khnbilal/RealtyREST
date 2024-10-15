d53d7ade0323da574e6c272c08cc771e47db46a4
Bilal Khan <khn.bilal17@gmail.com>
2024-10-16 00:51:31 +0530

    Update RealtyRepository.java

--- Backend/RealtyRepository.java
+++ Backend/RealtyRepository.java
@@ -3,7 +3,9 @@ package com.realestate.realtyrest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RealtyRepository extends JpaRepository<Realty, Long> {
-
+public List<Realty> findByNumberOfBedrooms(Long NumberOfBedrooms);
 }

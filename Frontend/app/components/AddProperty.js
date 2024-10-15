43e6e39fc506273274e068734904078880852a6e
Bilal Khan <khn.bilal17@gmail.com>
2024-10-16 00:42:11 +0530

    Update AddProperty.js

--- Frontend/app/components/AddProperty.js
+++ Frontend/app/components/AddProperty.js
@@ -1,10 +1,12 @@
 import AddPropertyForm from '../components/AddPropertyForm';
+import { addProperty } from '../components/apiService.js'; 
+import '../../styles/globals.css';
 
 const AddProperty = () => {
   return (
     <div>
       <h1>Add New Property</h1>
-      <AddPropertyForm />
+      <AddPropertyForm addProperty={addProperty} /> {/* Pass it as a prop */}
     </div>
   );
 };

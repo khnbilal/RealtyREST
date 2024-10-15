ae16f551f33fd4256987a58b018d8c7b8247841a
Bilal Khan <khn.bilal17@gmail.com>
2024-10-16 00:50:48 +0530

    Update Realty.java

--- Backend/Realty.java
+++ Backend/Realty.java
@@ -1,33 +1,56 @@
 package com.realestate.realtyrest;
 
 import jakarta.persistence.*;
-import lombok.Data; // Import Lombok Data annotation
+import lombok.Data;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "RealEstate")
-@Data // Automatically generates getters, setters, and constructors
+@Data
 public class Realty {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column(name="address")
-    private String address;
+    @Column(name = "location")
+    private String location; 
 
-    @Column(name="listingDate")
+    @Column(name = "houseFlatNo")
+    private String houseFlatNo; 
+
+    @Column(name = "building")
+    private String building; 
+
+    @Column(name = "locality")
+    private String locality; 
+
+    @Column(name = "listingDate")
     private LocalDate listingDate;
 
-    @Column(name="propertyType")
+    @Column(name = "propertyType")
     private String propertyType;
 
-    @Column(name="bedroom")
-    private int numberOfBedrooms; // Updated field name
+    @Column(name = "bedroom")
+    private int numberOfBedrooms;
 
-    @Column(name="bathroom")
-    private int numberOfBathrooms; // Updated field name
+    @Column(name = "bathroom")
+    private int numberOfBathrooms;
 
-    @Column(name="homeSize")
+    @Column(name = "homeSize")
     private float homeSize;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Lob // This annotation is for storing large objects
+    @Column(name = "propertyImage")
+    private byte[] propertyImage; // Field for storing image data
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
 }
+

e26123d846e04a2d85b68260d2f5a717e45fd20f
Bilal Khan <khn.bilal17@gmail.com>
2024-10-16 00:52:59 +0530

    Create GeocodingResult.java

---
+++ Backend/dto/GeocodingResult.java
@@ -0,0 +1,13 @@
+package com.realestate.realtyrest.dto;
+
+public class GeocodingResult {
+    private Geometry geometry;
+
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+}
